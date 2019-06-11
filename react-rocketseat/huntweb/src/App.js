179a0063f60b25200cb8e9d614333746041c238e
jonathanslima <jonathan633.jl@gmail.com>
2019-06-11 01:57:41 -0300

    call products via axios and show the titles at homepage

--- react-rocketseat/huntweb/src/App.js
+++ react-rocketseat/huntweb/src/App.js
@@ -5,12 +5,14 @@ import React from 'react';
 import './styles.css';
 
 /* Components ************************************/
-import Header from './components/Header/index';
+import Header from './components/Header';
+import Main from './pages/main';
 
 const App = ()=>{
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
--- react-rocketseat/huntweb/src/pages/main/index.js
+++ react-rocketseat/huntweb/src/pages/main/index.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
-import Api from '../../services/api';
+import api from '../../services/api';
 
-export default class Main extends Component{
+class Main extends Component{
+  state = {
+    products: []
+  }
+
+  // lifecycle for when show component
+  componentDidMount(){
+    // load products
+    this.loadProducts();
+  }
+
+  // using arrow function's syntax, you won't subscribe 'this' value
+  loadProducts = async ()=> {
+    const response = await api.get('/products');
+    this.setState({products: response.data.docs});
+    console.log(response)
+  }
+
   render(){
-    return <h1>Hello World!</h1>
+    return (
+      <div className="product-list">
+        {this.state.products.map(product => (
+          <h2>{product.title}</h2>
+        ))}
+      </div>
+    )
   }
 }
+
+export default Main;
 
