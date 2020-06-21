27bd9411195e4c603591ad15b915cb49e51f7e57
Lima <jonathan.da.s.lima@accenture.com>
2020-06-21 17:48:21 -0300

    putting a dinamic menu

--- course-react-firebase/src/components/navbar/index.js
+++ course-react-firebase/src/components/navbar/index.js
@@ -1,30 +1,77 @@
-import React from 'react';
-import './navbar.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./navbar.css";
+import { Link } from "react-router-dom";
+
+import { useDispatch, useSelector } from "react-redux";
 
 function Navbar() {
+	const dispatch = useDispatch();
 	return (
 		<>
-		<nav className="navbar navbar-expand-lg">
-			<span className="navbar-brand text-white font-weight-bold" >Eventos</span>
-			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<i className="fas fa-bars text-white"></i>
-			</button>
+			<nav className="navbar navbar-expand-lg">
+				<span className="navbar-brand text-white font-weight-bold">
+					Eventos
+				</span>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<i className="fas fa-bars text-white"></i>
+				</button>
 
-			<div className="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul className="navbar-nav mr-auto">
-					<li className="nav-item active">
-						<Link className="nav-link" to="/">Home </Link>
-					</li>
-					<li className="nav-item active">
-						<Link className="nav-link" to="/cadastrar">Cadastrar </Link>
-					</li>
-					<li className="nav-item active">
-						<Link className="nav-link" to="/login">Login </Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav mr-auto">
+						<li className="nav-item active">
+							<Link className="nav-link" to="/">
+								Home{" "}
+							</Link>
+						</li>
+						{useSelector((state) => state.usuarioLogado > 0) ? (
+							<>
+								<li className="nav-item active">
+									<Link className="nav-link" to="/">
+										Publicar Evento
+									</Link>
+								</li>
+								<li className="nav-item active">
+									<Link className="nav-link" to="/">
+										Meus Eventos
+									</Link>
+								</li>
+								<li className="nav-item active">
+									<Link
+										className="nav-link"
+										onClick={() => {
+											dispatch({ type: "LOG_OUT" });
+										}}
+										to=""
+									>
+										Sair
+									</Link>
+								</li>
+							</>
+						) : (
+							<>
+								<li className="nav-item active">
+									<Link className="nav-link" to="/cadastrar">
+										Cadastrar{" "}
+									</Link>
+								</li>
+								<li className="nav-item active">
+									<Link className="nav-link" to="/login">
+										Login{" "}
+									</Link>
+								</li>
+							</>
+						)}
+					</ul>
+				</div>
+			</nav>
 		</>
 	);
 }
--- course-react-firebase/src/store/usuarioReducer.js
+++ course-react-firebase/src/store/usuarioReducer.js
@@ -7,9 +7,9 @@ function usuarioReducer(state = INITIAL_STATE, action){
 	switch(action.type){
 		case 'LOG_IN':
 			return {...state, usuarioLogado: 1, usuarioEmail: action.usuarioEmail};
-		
+
 		case 'LOG_OUT':
-			return { ...state, usuarioLogado: 0, usuarioEmail: action.usuarioEmail };
+			return { ...state, usuarioLogado: 0, usuarioEmail: null };
 
 		default:
 			return state;
